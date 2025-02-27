b668f06f082ef06e46dc942dabf19a6d2d77ba64
Derek Allan <derek.allan@calastone.com>
2025-02-27 17:57:03 +0000

    uncomment order function

--- OrderReceivedFunction/OrderReceivedTrigger.cs
+++ OrderReceivedFunction/OrderReceivedTrigger.cs
@@ -1,50 +1,50 @@
-// using System.Text.Json;
-// using Azure.Messaging.EventGrid;
-// using Contracts;
-// using Microsoft.Azure.Functions.Worker;
-// using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using Azure.Messaging.EventGrid;
+using Contracts;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 
-// namespace Test.Orders;
+namespace Test.Orders;
 
-// public class OrderReceivedTrigger
-// {
-//     private readonly IMessageSession _messageSession;
-//     private readonly ILogger<OrderReceivedTrigger> _logger;
+public class OrderReceivedTrigger
+{
+    private readonly IMessageSession _messageSession;
+    private readonly ILogger<OrderReceivedTrigger> _logger;
 
-//     public OrderReceivedTrigger(
-//         IMessageSession messageSession,
-//         ILogger<OrderReceivedTrigger> logger)
-//     {
-//         _messageSession = messageSession;
-//         _logger = logger;
-//     }
+    public OrderReceivedTrigger(
+        IMessageSession messageSession,
+        ILogger<OrderReceivedTrigger> logger)
+    {
+        _messageSession = messageSession;
+        _logger = logger;
+    }
 
-//     [Function(nameof(OrderReceivedTrigger))]
-//     public async Task Run([EventGridTrigger] EventGridEvent eventGridEvent)
-//     {
-//         try
-//         {
-//             _logger.LogInformation("Event type: {type}, Event subject: {subject}", eventGridEvent.EventType, eventGridEvent.Subject);
-//             _logger.LogInformation("Data: {Data}", eventGridEvent.Data);
+    [Function(nameof(OrderReceivedTrigger))]
+    public async Task Run([EventGridTrigger] EventGridEvent eventGridEvent)
+    {
+        try
+        {
+            _logger.LogInformation("Event type: {type}, Event subject: {subject}", eventGridEvent.EventType, eventGridEvent.Subject);
+            _logger.LogInformation("Data: {Data}", eventGridEvent.Data);
 
-//             if (eventGridEvent.Data is not null)
-//             {
-//                 var order = JsonSerializer.Deserialize<Order>(eventGridEvent.Data.ToString(), new JsonSerializerOptions
-//                 {
-//                     PropertyNameCaseInsensitive = true
-//                 });
-//                 if (order is null)
-//                 {
-//                     _logger.LogWarning("Failed to deserialize the order data.");
-//                     return;
-//                 }
-//                 _logger.LogInformation("Order received: {orderId}, {orderDate}, {orderAmount}", order.OrderId, order.OrderDate, order.OrderAmount);
-//                 await _messageSession.Publish(order);
-//             }
-//         }
-//         catch (Exception ex)
-//         {
-//             _logger.LogError(ex, "An error occurred processing the order.");
-//         }
-//     }
-// }
+            if (eventGridEvent.Data is not null)
+            {
+                var order = JsonSerializer.Deserialize<Order>(eventGridEvent.Data.ToString(), new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+                if (order is null)
+                {
+                    _logger.LogWarning("Failed to deserialize the order data.");
+                    return;
+                }
+                _logger.LogInformation("Order received: {orderId}, {orderDate}, {orderAmount}", order.OrderId, order.OrderDate, order.OrderAmount);
+                await _messageSession.Publish(order);
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred processing the order.");
+        }
+    }
+}
